782f1bd6371835eff3543bc17c5e7396512421c0
Amitav Roy <reachme@amitavroy.com>
2013-04-27 19:53:00 +0530

    Will start with the clean plate now

--- application/modules/infscroll/views/ajax_listing_view.php
+++
@@ -1,9 +0,0 @@
-<?php foreach ($customers as $customer): ?>
-<tr>
-  <td><?php echo $customer['CompanyName']; ?></td>
-  <td><?php echo $customer['ContactName']; ?></td>
-  <td><?php echo $customer['ContactTitle']; ?></td>
-  <td><?php echo $customer['City']; ?></td>
-  <td><?php echo $customer['Country']; ?></td>
-</tr>
-<?php endforeach; ?>
--- application/modules/singlepage/views/singlepage_edit_view.php
+++
@@ -1,3 +0,0 @@
-<div ng-controller="bookEditCtrl">
-  <h3>Edit the Book</h3>
-</div>
