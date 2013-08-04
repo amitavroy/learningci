1fcaa5495c0b072a57db5d75c27a836333cfea8c
Amitav Roy <reachme@amitavroy.com>
2013-08-04 14:59:13 +0530

    Clearning up unnecessary files from branch

--- application/modules/notification/views/order_edit.php
+++
@@ -1,6 +0,0 @@
-<?php 
-dsm($order);
-echo form_open('notification/update_order');
-echo form_textarea('comment', $order['comments']);
-echo form_label();
-echo form_submit('submit', 'Save', 'class="btn btn-primary"'); ?>
--- application/views/products/products_index_view.php
+++
@@ -1,5 +0,0 @@
-<div class="row-fluid" ng-app="northWind">
-	
-	<div ng-view></div>
-
-</div>
